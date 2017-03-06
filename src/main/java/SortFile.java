0e61ff53a8372aa39e420633b3a91bb631fb04cb
KirillZubov <kirillzubov3@gmail.com>
2017-03-07 00:19:32 +0300

    create write and read methods

--- src/main/java/SortFile.java
+++ src/main/java/SortFile.java
@@ -1,7 +1,56 @@
 package main.java;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * Created by KirillZubov on 3/6/2017.
  */
 public class SortFile {
+
+    List<String> str = new ArrayList<>();
+
+
+    private void read(String source) throws IOException {
+        try {
+            File file = new File(source);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                str.add(line);
+            }
+            fileReader.close();
+        } catch (IOException e){
+            e.fillInStackTrace();
+        }
+
+    }
+
+    void dublicate() {
+        Collections.sort(str);
+
+    }
+
+    private void write(String target) {
+        try {
+            File fout = new File(target);
+            FileOutputStream fos = new FileOutputStream(fout);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+
+            for (String s : str) {
+                osw.write(s + '\n');
+            }
+
+            osw.close();
+        } catch (IOException e) {
+            e.fillInStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        SortFile sortFile = new SortFile();
+        sortFile.read("src\\main\\resource\\a.txt");
+        System.out.println(sortFile.str);
+    }
 }
